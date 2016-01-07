29db2922d13893e26375593c028c641ad27faf14
webmasteraxe <drop2axe@gmail.com>
2016-01-07 16:05:04 +0530

    Initial Commit

--- services/src/main/java/axyz/weezyboy/services/MainActivity.java
+++ services/src/main/java/axyz/weezyboy/services/MainActivity.java
@@ -1,7 +1,9 @@
 package axyz.weezyboy.services;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Method to start the service
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
     }
 }
---
+++ services/src/main/java/axyz/weezyboy/services/MyService.java
@@ -0,0 +1,29 @@
+package axyz.weezyboy.services;
+
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+/**
+ * Created by ${weezyboyaxy} on 1/7/2016.
+ */
+public class MyService extends Service {
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+    }
+}
