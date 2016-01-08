8ac131de7b61da084dab6ba1f0727e8416dc409c
webmasteraxe <drop2axe@gmail.com>
2016-01-08 18:09:17 +0530

    Initial Commit

--- services/src/main/java/axyz/weezyboy/services/activities/MainActivity.java
+++ services/src/main/java/axyz/weezyboy/services/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package axyz.weezyboy.services;
+package axyz.weezyboy.services.activities;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import axyz.weezyboy.services.MyService;
+import axyz.weezyboy.services.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ImageView imageView;
